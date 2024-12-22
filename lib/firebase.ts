fc91e620b5dee11c4e7ab71ba56cd43edc1cbe3a
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-12-22 13:27:24 +0000

    Fix the problem

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { getFirestore } from "firebase/firestore";
-import { initializeApp, getApps } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,7 +15,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = getApps().length === 0 ? initializeApp(firebaseConfig);
-const db = getFirestore(app)
+const app = getApps().length === 0 ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-export {db}
+export { db };
